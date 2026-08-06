50f9e38b7a2b465e8f03ceb043b0f8fb5264eb58
Archna Kumari <archnakumari.docs@gmail.com>
2026-08-06 17:18:10 +0530

    Leed Code Probleam No 4 Median Of Two Sorted Arrays

---
+++ src/LeetCodeDay_8_5_08_2026/CollectionDemo.java
@@ -0,0 +1,16 @@
+package LeetCodeDay_8_5_08_2026;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionDemo {
+
+ public static void main(String[] args) {
+	List<String> employee=new ArrayList<>();
+	employee.add("Archna  Kumari");
+	employee.add("Never");
+	employee.add("remover");
+	for(String name :employee) {
+	System.out.println(employee);
+}}
+}
