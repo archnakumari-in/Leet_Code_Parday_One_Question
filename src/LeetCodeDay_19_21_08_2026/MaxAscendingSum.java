07f98047ca76e4457415b16a7025180de2988d0e
Archna Kumari <archnakumari.docs@gmail.com>
2026-08-21 12:38:06 +0530

    MaxAscendingSum

---
+++ src/LeetCodeDay_19_21_08_2026/MaxAscendingSum.java
@@ -0,0 +1,37 @@
+package LeetCodeDay_19_21_08_2026;
+
+public class MaxAscendingSum {
+
+    public static int maxAscendingSum(int[] nums) {
+
+        int max = nums[0];
+        int currmax = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[i - 1] < nums[i]) {
+
+                currmax = currmax + nums[i];
+
+            } else {
+
+                max = Math.max(max, currmax);
+
+                currmax = nums[i];
+            }
+        }
+
+        max = Math.max(max, currmax);
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {10, 20, 30, 5, 10, 50};
+
+        int result = maxAscendingSum(nums);
+
+        System.out.println("Maximum Ascending Sum = " + result);
+    }
+}
