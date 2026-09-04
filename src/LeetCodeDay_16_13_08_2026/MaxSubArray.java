837f33a7e949897e237dd054f36419f0ac1a077c
Archna Kumari <archnakumari.docs@gmail.com>
2026-09-04 10:27:21 +0530

    Adding Tokon

---
+++ src/LeetCodeDay_16_13_08_2026/MaxSubArray.java
@@ -0,0 +1,29 @@
+package LeetCodeDay_16_13_08_2026;
+
+public class MaxSubArray {
+
+    public int maxSubArray(int[] nums) {
+
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        System.out.println("Starting currentSum = " + currentSum);
+        System.out.println("Starting maxSum = " + maxSum);
+
+        for (int i = 1; i < nums.length; i++) {
+
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+
+            maxSum = Math.max(maxSum, currentSum);
+
+            System.out.println(
+                "i = " + i +
+                ", nums[i] = " + nums[i] +
+                ", currentSum = " + currentSum +
+                ", maxSum = " + maxSum
+            );
+        }
+
+        return maxSum;
+    }
+}
